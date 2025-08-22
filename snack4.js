7c96e4e8d0e9da7fe133b923bf5d4f10fd5d25a9
EneaSema <sema.enea1993@gmail.com>
2025-08-22 17:34:12 +0200

    resolve bonus 2

--- snack4.js
+++ snack4.js
@@ -1,7 +1,65 @@
+// async function getChefBirthday(id) {
+//   let responseFetch;
+//   try {
+//     responseFetch = await fetch(`https://dummyjson.com/recipes/${id}`);
+//     const resolve = await responseFetch.json();
+//     console.log(resolve);
+//     const userId = resolve.userId;
+
+//     console.log(userId);
+//     const responseFetchUser = await fetch(
+//       `https://dummyjson.com/users/${userId}`
+//     );
+//     const resolveUser = await responseFetchUser.json();
+//     console.log(resolveUser);
+//     const birthday = resolveUser.birthDate;
+//     console.log(birthday);
+//     return birthday;
+//   } catch (error) {
+//     console.error(`Errore:`, error.messagge);
+//   }
+// }
+
+// getChefBirthday(1)
+//   .then((birthday) => console.log(`La data di nascità dello chef è:`, birthday))
+//   .catch((error) => console.error(`Errore.`, error.messagge));
+
+// BONUS 1
+
+// async function getChefBirthday(id) {
+//   let responseFetch;
+//   try {
+//     responseFetch = await fetch(`https://dummyjson.com/recipes/${id}`);
+
+//     const resolve = await responseFetch.json();
+//     console.log(resolve);
+//     const userId = resolve.userId;
+
+//     console.log(userId);
+//     const responseFetchUser = await fetch(
+//       `https://dummyjson.com/users/${userId}`
+//     );
+//     const resolveUser = await responseFetchUser.json();
+//     console.log(resolveUser);
+//     const birthday = resolveUser.birthDate;
+//     console.log(birthday);
+//     return birthday;
+//   } catch (error) {
+//     console.error(`Errore:`, error.messagge);
+//   }
+// }
+
+// getChefBirthday(1)
+//   .then((birthday) => console.log(`La data di nascità dello chef è:`, birthday))
+//   .catch((error) => console.error(`Errore.`, error.messagge));
+
+// BONUS 2
+
 async function getChefBirthday(id) {
   let responseFetch;
   try {
     responseFetch = await fetch(`https://dummyjson.com/recipes/${id}`);
+
     const resolve = await responseFetch.json();
     console.log(resolve);
     const userId = resolve.userId;
@@ -12,14 +70,14 @@ async function getChefBirthday(id) {
     );
     const resolveUser = await responseFetchUser.json();
     console.log(resolveUser);
-    const birthday = resolveUser.birthDate;
-    console.log(birthday);
-    return birthday;
+    const birthday = dayjs(resolveUser.birthDate);
+    const formatBirthday = birthday.format(`DD/MM/YYYY`);
+    return formatBirthday;
   } catch (error) {
     console.error(`Errore:`, error.messagge);
   }
 }
 
-getChefBirthday(1)
+getChefBirthday(3)
   .then((birthday) => console.log(`La data di nascità dello chef è:`, birthday))
   .catch((error) => console.error(`Errore.`, error.messagge));
