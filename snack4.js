37f3e4d3db1ac331313224ee3da27ee42c435999
EneaSema <sema.enea1993@gmail.com>
2025-08-22 14:29:33 +0200

    aggiungo try e catch

---
+++ snack4.js
@@ -0,0 +1,20 @@
+async function getChefBirthday(id) {
+  const responseFetch = await fetch(`https://dummyjson.com/recipes/${id}`);
+  const resolve = await responseFetch.json();
+  console.log(resolve);
+  const userId = resolve.userId;
+
+  console.log(userId);
+  const responseFetchUser = await fetch(
+    `https://dummyjson.com/users/${userId}`
+  );
+  const resolveUser = await responseFetchUser.json();
+  console.log(resolveUser);
+  const birthday = resolveUser.birthDate;
+  console.log(birthday);
+  return birthday;
+}
+
+getChefBirthday(1)
+  .then((birthday) => console.log(`La data di nascità dello chef è:`, birthday))
+  .catch((error) => console.error(`Errore.`, error.messagge));
