57260a121f3119136ea7be1d9d87d27f62a39511
EneaSema <sema.enea1993@gmail.com>
2025-08-22 15:27:42 +0200

    resolve snack4 base

--- snack4.js
+++ snack4.js
@@ -1,18 +1,23 @@
 async function getChefBirthday(id) {
-  const responseFetch = await fetch(`https://dummyjson.com/recipes/${id}`);
-  const resolve = await responseFetch.json();
-  console.log(resolve);
-  const userId = resolve.userId;
+  let responseFetch;
+  try {
+    responseFetch = await fetch(`https://dummyjson.com/recipes/${id}`);
+    const resolve = await responseFetch.json();
+    console.log(resolve);
+    const userId = resolve.userId;
 
-  console.log(userId);
-  const responseFetchUser = await fetch(
-    `https://dummyjson.com/users/${userId}`
-  );
-  const resolveUser = await responseFetchUser.json();
-  console.log(resolveUser);
-  const birthday = resolveUser.birthDate;
-  console.log(birthday);
-  return birthday;
+    console.log(userId);
+    const responseFetchUser = await fetch(
+      `https://dummyjson.com/users/${userId}`
+    );
+    const resolveUser = await responseFetchUser.json();
+    console.log(resolveUser);
+    const birthday = resolveUser.birthDate;
+    console.log(birthday);
+    return birthday;
+  } catch (error) {
+    console.error(`Errore:`, error.messagge);
+  }
 }
 
 getChefBirthday(1)
